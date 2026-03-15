92abea9fdb15ef3827f5b98bba7ee62db5632c71
EMIX028 <maxime.chl2007@gmail.com>
2026-03-15 19:19:22 +0100

    some changes in char_BPE to improve performance

--- test/char_BPE.c
+++ test/char_BPE.c
@@ -6,105 +6,95 @@
 
 
 int main(void){
-    
     size_t capacity = 1;
     char **key = malloc(capacity*sizeof(char *));
     int *value = malloc(capacity*sizeof(int));
 
-    char mot[] = "aqbbaabb";
+    char mot[] = "agbfk86k8Yakbfqazjvfhjgbkberbnjkedgnegkerjgerjhlnf";
+
     size_t L_mot = sizeof(mot)/sizeof(mot[0])-1;
-    Dict dico = {key,value,0};
+    Dict dico = {key, value, 0, capacity};
     
-    int res = ch_to_dict(mot,L_mot,&dico,&capacity);
+    int res = ch_to_dict(mot,L_mot,&dico);
     if (res == -1){
         fprintf(stderr,"Erreur d'allocation mémoire");
         return EXIT_FAILURE;
     }
 
     printf("{");
-    for (int i =0; i<dico.count; i++){
-        printf("%s : %d, ",dico.key[i],dico.value[i]);
+    char **ptr_key = dico.key;
+    int *ptr_val = dico.value;
+    for (size_t i = 0; i < dico.count; i++) {
+        printf("'%s' : %d,", *ptr_key, *ptr_val);
+        ++ptr_key;
+        ++ptr_val;
     }
+    //printf("\n -> %ld\n",dico.capacity);
+    //printf("\n -> %ld\n",dico.count);
     printf("}\n");
 
-    free(dico.key);
-    free(dico.value);
 
+    for (char **ptr_Key = dico.key; ptr_Key < dico.key + dico.count; ++ptr_Key) {
+        free(*ptr_Key);
+    }
+    free(dico.key);
+    free(dico.value);   
+
+    
     return EXIT_SUCCESS;
 }
 
+
 bool ch_in_key(Dict *dico,const char *ch, size_t *k){
+    char **ptr_dico = dico->key;
     size_t i = 0;
     while (i < dico->count){
-        if ( strcmp(ch , dico->key[i])==0){
+        if ( strcmp(ch , *ptr_dico)==0){
             *k = i;
             return true;
         }
+        ++ptr_dico;
         ++i;
     }
     return false;
 }
 
 
-static int ch_to_dict( char mot[], size_t L_mot, Dict *dico, size_t *capacity){
+static int ch_to_dict( char mot[], size_t L_mot, Dict *dico){
     char s[3];
     size_t k;
 
-    
-    if( L_mot%2 != 0 ){
-        for (size_t i=0; i<L_mot; ++i){
-            if ( i != (L_mot-1) ){
-                sprintf(s,"%c%c",mot[i],mot[i+1]);
-                if (ch_in_key(dico,s,&k)==1){
-                    dico->value[k] += 1; 
-                }
-                else{
-                    ++(*capacity);
-                    char **newAllocKey = realloc(dico->key, (*capacity)*sizeof(*dico->key));
-                    if (newAllocKey == NULL ){
-                        return -1;
-                    }
-                    int *newAllocVal = realloc(dico->value, (*capacity)*sizeof(*dico->value));
-                    if (newAllocVal == NULL ){
+    for (size_t i=0; i<L_mot; ++i){
+        //printf("%ld\n",dico->capacity);
+        if ( i != (L_mot-1) ){
+            sprintf(s,"%c%c",mot[i],mot[i+1]);
+            if (ch_in_key(dico,s,&k) == true){
+                dico->value[k] += 1; 
+            }
+            else{
+                if (dico->count == dico->capacity){
+                    size_t newCapacity = dico->capacity * 2;
+                
+                    char **newAllocKey = realloc(dico->key, newCapacity*sizeof(*dico->key));
+                    int *newAllocVal = realloc(dico->value, newCapacity*sizeof(*dico->value));
+                    if (newAllocKey == NULL || newAllocVal == NULL ){
+                        fprintf(stderr,"Erreur lors de la réallocation");
                         return -1;
                     }
                     dico->key = newAllocKey;
                     dico->value = newAllocVal;
+                    dico->capacity= newCapacity;
+                }
                     dico->key[dico->count] = strdup(s);
                     dico->value[dico->count] = 1;
                     dico->count++;
-                }
-            }
-        }
-        char lastChar[2] = {mot[L_mot-1], '\0'};
-        dico->key[*capacity-1] = strdup(lastChar);
-        dico->value[*capacity-1] = -1;
-    }
-    else{
-        for (size_t i=0; i<L_mot-1; ++i){
-            if ( i != (L_mot-1) ){
-                sprintf(s,"%c%c",mot[i],mot[i+1]);
-                if (ch_in_key(dico,s,&k)==1){
-                    dico->value[k] += 1; 
-                }
-                else{
-                    ++capacity;
-                    char **newAllocKey = realloc(dico->key, (*capacity)*sizeof(*dico->key));
-                    if (newAllocKey == NULL ){
-                        return -1;
-                    }
-                    int *newAllocVal = realloc(dico->value, (*capacity)*sizeof(*dico->value));
-                    if (newAllocVal == NULL ){
-                        return -1;
-                    }
-                    dico->key = newAllocKey;
-                    dico->value = newAllocVal;
-                    dico->key[dico->count] = strdup(s);
-                    dico->value[dico->count] = 1;
-                    dico->count++;
-                }       
             }
         }
     }
+        if( L_mot%2 != 0 ){
+            char lastChar[2] = {mot[L_mot-1], '\0'};
+            dico->key[dico->count-1] = strdup(lastChar);
+            dico->value[dico->count-1] = -1;
+        }
 return 0;
 }
--- test/char_BPE_init.h
+++ test/char_BPE_init.h
@@ -2,8 +2,9 @@ typedef struct{
     char **key;
     int *value;
     size_t count;
+    size_t capacity;
 } Dict;
 
 bool ch_in_key(Dict *dico,const char *ch, size_t *k);
 
-static int ch_to_dict( char mot[], size_t L_mot, Dict *dico, size_t *capacity);
+static int ch_to_dict( char mot[], size_t L_mot, Dict *dico);
